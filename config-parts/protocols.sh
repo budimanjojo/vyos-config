03586c148fcfcd68ece05ea21a4d6b6b7e43daf0
budimanjojo <budimanjojo@gmail.com>
2023-07-04 19:44:05 +0700

    fix: not interface name

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -7,12 +7,12 @@ set protocols bgp parameters router-id '192.168.10.1'
 set protocols bgp neighbor 192.168.200.21 description 'kmaster1'
 set protocols bgp neighbor 192.168.200.21 remote-as '65401'
 set protocols bgp neighbor 192.168.200.21 address-family ipv4-unicast
-set protocols bgp neighbor 192.168.200.21 update-source SERVER
+set protocols bgp neighbor 192.168.200.21 update-source eth1.200
 set protocols bgp neighbor 192.168.200.22 description 'kmaster2'
 set protocols bgp neighbor 192.168.200.22 remote-as '65401'
 set protocols bgp neighbor 192.168.200.22 address-family ipv4-unicast
-set protocols bgp neighbor 192.168.200.22 update-source SERVER
+set protocols bgp neighbor 192.168.200.22 update-source eth1.200
 set protocols bgp neighbor 192.168.200.23 description 'kmaster3'
 set protocols bgp neighbor 192.168.200.23 remote-as '65401'
 set protocols bgp neighbor 192.168.200.23 address-family ipv4-unicast
-set protocols bgp neighbor 192.168.200.24 update-source SERVER
+set protocols bgp neighbor 192.168.200.24 update-source eth1.200
