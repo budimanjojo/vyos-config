4c7425a046e7e6b5f378f226a34e524d81b24ac2
budimanjojo <budimanjojo@gmail.com>
2023-07-06 10:27:55 +0700

    feat: use a more decent ntp server

--- config-parts/service.sh
+++ config-parts/service.sh
@@ -12,6 +12,8 @@ set service ntp allow-client address '127.0.0.0/8'
 set service ntp allow-client address '10.0.0.0/8'
 set service ntp allow-client address '172.16.0.0/12'
 set service ntp allow-client address '192.168.0.0/16'
+delete service ntp server
+set service ntp server id.pool.ntp.org
 
 # LAN1 DHCP server
 set service dhcp-server shared-network-name LAN1 authoritative
