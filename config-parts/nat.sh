61e13f15229dea6020ca6865742520eb68ebf7a0
budimanjojo <budimanjojo@gmail.com>
2023-07-06 19:29:47 +0700

    fix: dumb typo

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -6,7 +6,7 @@ set nat destination rule 101 description 'Port forward 80,443 to k8s Ingress'
 set nat destination rule 101 destination port '80,443'
 set nat destination rule 101 protocol 'tcp'
 set nat destination rule 101 inbound-interface 'eth0'
-set nat destination rule 101 translation address '192.168.5.0'
+set nat destination rule 101 translation address '192.168.15.0'
 
 # Force DNS
 set nat destination rule 102 description 'Force DNS for IOT VLAN'
