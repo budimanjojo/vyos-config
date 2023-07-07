459bcd98892c1fa8bd7e0ed0105c4cd00523f211
budimanjojo <budimanjojo@gmail.com>
2023-07-07 11:52:30 +0700

    feat: I don't understand now

--- config-parts/qos.sh
+++ config-parts/qos.sh
@@ -9,4 +9,4 @@ set qos policy shaper UPLOAD bandwidth 15mbit
 set qos policy shaper UPLOAD default bandwidth 100%
 set qos policy shaper UPLOAD default queue-type fq-codel
 
-set qos interface eth0 egress UPLOAD
+set qos interface eth0 egress DOWNLOAD
