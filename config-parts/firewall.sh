7a78ce7a17dd900f2fe8ab3269028a9e701c60ac
budimanjojo <budimanjojo@gmail.com>
2024-08-25 13:42:17 +0700

    fix: typo
    Signed-off-by: budimanjojo <budimanjojo@gmail.com>

--- config-parts/firewall.sh
+++ config-parts/firewall.sh
@@ -17,7 +17,7 @@ set firewall group address-group wall_displays address '192.168.50.40'
 set firewall group address-group k8s_api address '192.168.200.20'
 set firewall group address-group k8s_ingress address '192.168.15.0'
 set firewall group address-group k8s_vector_aggregator address '192.168.15.5'
-set firewall group address-group k8s_qtbittorrent address '192.168.15.10'
+set firewall group address-group k8s_qbittorrent address '192.168.15.10'
 
 set firewall group address-group k8s_services address '192.168.15.0-192.168.15.254'
 set firewall group address-group k8s_services address '192.168.200.200-192.168.200.210'
